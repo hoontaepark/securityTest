0cb2ec0c3d31bdb9cc991e54ea13b4ce925a7c4b
hoontaepark <pthomj13@gmail.com>
2024-05-02 10:37:40 +0900

    springSecurityConfig

---
+++ src/main/java/com/example/securityTest/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.securityTest.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean //시큐리티 필터체인 빈 등록
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http    //변수는 람다식으로 선언, 특정 경로에 요청을 진행함.
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/","/login").permitAll()//특정 경로에 요청을 진행함.
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("USER","ADMIN") //유저의 경로는 **와일드카드로 선언.
+                        .anyRequest().authenticated() //이외 경로의 대한 처리 로그인한 사용자만 접근할수있도록 authenticated
+                ); //특정권한에 대한 접근을 선언.
+
+        return http.build(); //리턴값으로 http리턴.
+
+
+    }
+
+
+}
